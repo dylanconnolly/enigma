e3ec732dd5c1553c6f96f1b6f58191f47c17e2b9
Dylan Connolly <dylanconnolly145@gmail.com>
2019-11-01 12:07:28 -0600

    Create set_keys method

--- lib/keys.rb
+++ lib/keys.rb
@@ -2,12 +2,22 @@ class Keys
   attr_reader :keys
 
   def initialize(rand_number = '%05d' % rand(99999))
-    @keys = {
-      "A" => 0,
-      "B" => 0,
-      "C" => 0,
-      "D" => 0
-    }
     @random_number = rand_number
+    @keys = {
+      "A" => nil,
+      "B" => nil,
+      "C" => nil,
+      "D" => nil
+    }
+  end
+
+  def set_keys
+    element_array = @random_number.split("")
+    @keys = {
+      "A" => (element_array[0] + element_array[1]).to_i,
+      "B" => (element_array[1] + element_array[2]).to_i,
+      "C" => (element_array[2] + element_array[3]).to_i,
+      "D" => (element_array[3] + element_array[4]).to_i
+    }
   end
 end
