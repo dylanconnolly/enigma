529120befb60214ddf55f06130aa21c37721245b
Dylan Connolly <dylanconnolly145@gmail.com>
2019-11-01 11:39:01 -0600

    Create initialize test for keys class

--- test/keys_test.rb
+++ test/keys_test.rb
@@ -1,4 +1,5 @@
 require './test/test_help'
+require './lib/keys'
 
 class KeysTest < Minitest::Test
 
@@ -9,4 +10,13 @@ class KeysTest < Minitest::Test
   def test_it_exists
     assert_instance_of Keys, @keys
   end
+
+  def test_it_initializes_with_hash_of_keys
+    expected = {
+      "A" => 0,
+      "B" => 0,
+      "C" => 0,
+      "D" => 0
+    }
+    assert_equal expected, @keys.keys
 end
