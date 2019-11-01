c0d14f0ea1ddc0dffdc25222c12dc29c4ef77c76
Dylan Connolly <dylanconnolly145@gmail.com>
2019-11-01 12:11:26 -0600

    Create offset file and add tests for it exists

---
+++ test/offset_test.rb
@@ -0,0 +1,13 @@
+require './test/test_helper'
+require './lib/offset'
+
+class OffsetTest < Minitest::Test
+
+  def setup
+    @offset = Offset.new
+  end
+
+  def test_it_exists
+    assert_instance_of Offset, @offset
+  end
+end
