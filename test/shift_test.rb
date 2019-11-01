603b3ba548cb1e53f67511215755620c32e1a652
Dylan Connolly <dylanconnolly145@gmail.com>
2019-11-01 12:25:43 -0600

    Remove random number method from shift class, it now only initializes with alphabet

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -15,8 +15,3 @@ class ShiftTest < Minitest::Test
     expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
     assert_equal expected, @shift.alphabet
   end
-
-  def test_it_generates_a_random_five_digit_number
-    assert_equal 5, @shift.random_key.length
-  end
-end
