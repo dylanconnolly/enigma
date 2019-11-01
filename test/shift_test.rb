fc1b6e6f6e174ec92330451b418db72b5c57b708
Dylan Connolly <dylanconnolly145@gmail.com>
2019-11-01 12:27:53 -0600

    Add end to shift test, rake runs clean

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -15,3 +15,4 @@ class ShiftTest < Minitest::Test
     expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
     assert_equal expected, @shift.alphabet
   end
+end
