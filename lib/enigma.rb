64aa8c99868bee36445732688bca63400703b129
Dylan Connolly <dylanconnolly145@gmail.com>
2019-11-05 12:00:11 -0700

    Add Shift to encrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,6 +1,7 @@
 class Enigma
 
-  def encrypt(message, key, date)
+  def encrypt(message, key = '%05d' % rand(99999), date)
+    Shift.set_shift(KeyGenerator.set_keys(key), OffsetGenerator.set_offset(date))
   end
 
 end
