c1e11ba3816c900d9d8d47d58172c29159e1225a
Dylan Connolly <dylanconnolly145@gmail.com>
2019-11-04 18:57:38 -0700

    Add test for shift class to test it can combine the keys and offset amounts

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -1,5 +1,7 @@
 require './test/test_helper'
 require './lib/shift'
+require './lib/keys'
+require './lib/offset'
 
 class ShiftTest < Minitest::Test
 
@@ -14,5 +16,18 @@ class ShiftTest < Minitest::Test
   def test_it_initializes_with_alphabet_array
     expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
     assert_equal expected, @shift.alphabet
+  end
+
+  def test_it_can_set_shift_values
+    keys = Keys.new("12345")
+    offset = Offset.new("010919")
+
+    expected = {
+      "A" => 16,
+      "B" => 28,
+      "C" => 40,
+      "D" => 46
+    }
+    assert_equal expected, @shift.set_shift(keys.set_keys, offset.set_offset)
   end
 end
