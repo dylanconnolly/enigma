14d7fbba27b58321a28ef6bd9e0a3b1a5053d230
Dylan Connolly <dylanconnolly145@gmail.com>
2019-11-05 13:24:55 -0700

    Create encrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,17 +1,43 @@
 class Enigma
-  attr_reader :characters
+  attr_reader :alphabet
 
   def initialize
-    @characters = ("a".."z").to_a << " "
+    @alphabet = ("a".."z").to_a << " "
   end
 
   def encrypt(message, key = '%05d' % rand(99999), date)
     shift = apply_shift(key, date)
 
+    encrypted = message.chars.map.with_index do |character, index|
+      position = @alphabet.index(character)
+      if position == nil
+        character
+      elsif index % 4 == 0
+        new_alpha = @alphabet.rotate(shift["A"] % 27)
+        new_alpha[position]
+      elsif index % 4 == 1
+        new_alpha = @alphabet.rotate(shift["B"] % 27)
+        new_alpha[position]
+      elsif index % 4 == 2
+        new_alpha = @alphabet.rotate(shift["C"] % 27)
+        new_alpha[position]
+      elsif index % 4 == 3
+        new_alpha = @alphabet.rotate(shift["D"] % 27)
+        new_alpha[position]
+      end
+    end
+
+    output = {
+      encryption: encrypted.join,
+      key: key,
+      date: date
+    }
+    output
   end
 
   def apply_shift(key, date)
     Shift.set_shift(KeyGenerator.set_keys(key), OffsetGenerator.set_offset(date))
   end
+
 
 end
