1b828d74af361e523ccb90aee7ffec3bb01ac111
Dylan Connolly <dylanconnolly145@gmail.com>
2019-11-05 14:04:23 -0700

    Create decrypt method, passing test

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -35,6 +35,36 @@ class Enigma
     output
   end
 
+  def decrypt(message, key, date)
+    shift = apply_shift(key, date)
+
+    decrypted = message.chars.map.with_index do |character, index|
+      position = @alphabet.index(character)
+      if position == nil
+        character
+      elsif index % 4 == 0
+        new_alpha = @alphabet.rotate(-(shift["A"] % 27))
+        new_alpha[position]
+      elsif index % 4 == 1
+        new_alpha = @alphabet.rotate(-(shift["B"] % 27))
+        new_alpha[position]
+      elsif index % 4 == 2
+        new_alpha = @alphabet.rotate(-(shift["C"] % 27))
+        new_alpha[position]
+      elsif index % 4 == 3
+        new_alpha = @alphabet.rotate(-(shift["D"] % 27))
+        new_alpha[position]
+      end
+    end
+
+    output = {
+      decryption: decrypted.join,
+      key: key,
+      date: date
+    }
+    output
+  end
+
   def apply_shift(key, date)
     Shift.set_shift(KeyGenerator.set_keys(key), OffsetGenerator.set_offset(date))
   end
