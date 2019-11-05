b485a722033074e6a0be72c20c4d99388c7933c5
Dylan Connolly <dylanconnolly145@gmail.com>
2019-11-05 14:20:17 -0700

    Pull conditional if statements into rotate helper method for enigma

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -8,24 +8,27 @@ class Enigma
   def encrypt(message, key = '%05d' % rand(99999), date = Time.now.strftime("%d%m%y"))
     shift = apply_shift(key, date)
 
-    encrypted = message.chars.map.with_index do |character, index|
-      position = @alphabet.index(character)
-      if position == nil
-        character
-      elsif index % 4 == 0
-        new_alpha = @alphabet.rotate(shift["A"] % 27)
-        new_alpha[position]
-      elsif index % 4 == 1
-        new_alpha = @alphabet.rotate(shift["B"] % 27)
-        new_alpha[position]
-      elsif index % 4 == 2
-        new_alpha = @alphabet.rotate(shift["C"] % 27)
-        new_alpha[position]
-      elsif index % 4 == 3
-        new_alpha = @alphabet.rotate(shift["D"] % 27)
-        new_alpha[position]
-      end
-    end
+    encrypted = rotate(message, shift)
+    
+
+    # encrypted = message.chars.map.with_index do |character, index|
+    #   position = @alphabet.index(character)
+    #   if position == nil
+    #     character
+    #   elsif index % 4 == 0
+    #     new_alpha = @alphabet.rotate(shift["A"] % 27)
+    #     new_alpha[position]
+    #   elsif index % 4 == 1
+    #     new_alpha = @alphabet.rotate(shift["B"] % 27)
+    #     new_alpha[position]
+    #   elsif index % 4 == 2
+    #     new_alpha = @alphabet.rotate(shift["C"] % 27)
+    #     new_alpha[position]
+    #   elsif index % 4 == 3
+    #     new_alpha = @alphabet.rotate(shift["D"] % 27)
+    #     new_alpha[position]
+    #   end
+    # end
 
     output = {
       encryption: encrypted.join,
@@ -63,6 +66,27 @@ class Enigma
       date: date
     }
     output
+  end
+
+  def rotate(message, shift)
+    message.chars.map.with_index do |character, index|
+      position = @alphabet.index(character)
+      if position == nil
+        character
+      elsif index % 4 == 0
+        new_alpha = @alphabet.rotate(shift["A"] % 27)
+        new_alpha[position]
+      elsif index % 4 == 1
+        new_alpha = @alphabet.rotate(shift["B"] % 27)
+        new_alpha[position]
+      elsif index % 4 == 2
+        new_alpha = @alphabet.rotate(shift["C"] % 27)
+        new_alpha[position]
+      elsif index % 4 == 3
+        new_alpha = @alphabet.rotate(shift["D"] % 27)
+        new_alpha[position]
+      end
+    end
   end
 
   def apply_shift(key, date)
