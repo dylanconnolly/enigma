0c739692a7dbb5ca12a69575e211fb00a037d69b
Dylan Connolly <dylanconnolly145@gmail.com>
2019-11-05 11:36:03 -0700

    Refactor tests to only test for set_keys class method

--- test/offset_generator_test.rb
+++ test/offset_generator_test.rb
@@ -1,27 +1,7 @@
 require './test/test_helper'
 require './lib/offset_generator'
 
-class OffsetTest < Minitest::Test
-
-  def setup
-    @offset_generator = OffsetGenerator.new("010919")
-  end
-
-  def test_it_exists
-    assert_instance_of OffsetGenerator, @offset_generator
-  end
-
-  def test_it_intializes_with_a_date
-    assert_equal "010919", @offset_generator.date
-  end
-
-  def test_it_can_square_the_date
-    assert_equal 119_224_561, @offset_generator.square_date
-  end
-
-  def test_it_can_get_last_four_digits_of_squared_date
-    assert_equal ["4","5","6","1"], @offset_generator.last_four
-  end
+class OffsetGeneratorTest < Minitest::Test
 
   def test_set_offset
     expected = {
@@ -30,6 +10,6 @@ class OffsetTest < Minitest::Test
       "C" => 6,
       "D" => 1
     }
-    assert_equal expected, @offset_generator.set_offset
+    assert_equal expected, OffsetGenerator.set_offset("010919")
   end
 end
