ad19b8944ada9084da171dd2f6661e71ed91b97d
Dylan Connolly <dylanconnolly145@gmail.com>
2019-11-05 12:22:49 -0700

    Add initialize to enigma with attribute of all 27 characters. Add helper method apply_shift to encrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,6 +1,16 @@
 class Enigma
+  attr_reader :characters
+
+  def initialize
+    @characters = ("a".."z").to_a << " "
+  end
 
   def encrypt(message, key = '%05d' % rand(99999), date)
+    shift = apply_shift(key, date)
+
+  end
+
+  def apply_shift(key, date)
     Shift.set_shift(KeyGenerator.set_keys(key), OffsetGenerator.set_offset(date))
   end
 
