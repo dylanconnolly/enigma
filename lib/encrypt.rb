a4f4efa6efe8d874a3d102f3f6c82c334184fc85
Dylan Connolly <dylanconnolly145@gmail.com>
2019-11-05 23:36:39 -0700

    Create CLI to open message file and write encrypted message to encrypted file

---
+++ lib/encrypt.rb
@@ -0,0 +1,17 @@
+require './lib/enigma'
+require './lib/key_generator'
+require './lib/offset_generator'
+require './lib/shift'
+
+file = File.open(ARGV[0], "r")
+message = file.read.chomp
+file.close
+
+enigma = Enigma.new
+
+encryption = enigma.encrypt(message)
+
+new_file = File.open(ARGV[1], "w")
+new_file.write(encryption[:encryption])
+
+puts "Created '#{ARGV[1]}' with the key #{encryption[:key]} and date #{encryption[:date]}."
